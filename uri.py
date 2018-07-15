5a7d82875dd8ae4b679e4f33c058b1de521c7db0
Brendan Jurd <direvus@gmail.com>
2018-07-15 21:47:54 +1000

    Add new module for generating key URIs.

---
+++ uri.py
@@ -0,0 +1,23 @@
+"""Generate key URIs for TOTP."""
+from base64 import b32encode
+from urllib import urlencode, quote
+
+
+def key_uri(key, issuer, accountname, digits, step):
+    """Return a TOTP URI for importing a key into a token application.
+
+    The URI is produced according to Google Authenticator's Key URI format [1],
+    and is suitable for rendering as a QR code.
+
+    [1] https://github.com/google/google-authenticator/wiki/Key-Uri-Format
+    """
+    params = {
+        'secret': b32encode(key),
+        'issuer': issuer,
+        'algorithm': 'SHA1',
+        'digits': digits,
+        'period': step}
+    return 'otpauth://totp/{}:{}?{}'.format(
+        quote(issuer),
+        quote(accountname),
+        urlencode(params))
